ea898254ab9e099427da35b47acd36a8fb57a3db
Darthex <mrharshulsharma@gmail.com>
2025-03-21 02:17:14 +0530

    test

--- app/database/database.py
+++ app/database/database.py
@@ -2,7 +2,7 @@ import logging
 
 from sqlalchemy import create_engine, Engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import Session, sessionmaker, scoped_session
 
 logger = logging.getLogger(__name__)
 
@@ -10,18 +10,18 @@ Base = declarative_base()
 
 
 class Database:
-    session = None
+    engine = None
 
     def __init__(
         self, db_url: str, echo: bool = False, echo_pool: bool = False
     ) -> None:
-        if Database.session is None:
+        if Database.engine is None:
             logger.info("Initializing Postgres Engine")
             self._engine = create_engine(db_url, echo=echo, echo_pool=echo_pool)
-            Database.session = self._resolve_session(self._engine)
+            Database.engine = self._engine
 
     @staticmethod
-    def _resolve_session(engine: Engine) -> Session:
-        return sessionmaker(
+    def resolve_session(engine: Engine) -> Session:
+        return scoped_session(sessionmaker(
             bind=engine, autoflush=False, autocommit=False, expire_on_commit=False
-        )()
+        ))()
--- app/database/sql_client.py
+++ app/database/sql_client.py
@@ -1,3 +1,5 @@
+from contextlib import contextmanager
+
 from app.database.database import Database
 from concurrent.futures import ThreadPoolExecutor
 
@@ -6,13 +8,16 @@ executor = ThreadPoolExecutor(max_workers=5)
 
 
 class SQLClient:
+    _client_engine = None
+
     def __init__(self, db: Database):
         self.db = db
+        SQLClient._client_engine = Database.engine
 
     @staticmethod
     def handle_session(func):
         def wrapper(self, *args, **kwargs):
-            with self.client_factory.db.session as session:
+            with Database.resolve_session(SQLClient._client_engine) as session:
                 try:
                     # only use positional arguments in mixin function
                     result = func(self, session=session, **kwargs)
