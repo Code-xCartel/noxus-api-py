a0bf356b98f59445ee0e05d614c01bcf3ddb6e90
Darthex <mrharshulsharma@gmail.com>
2025-04-30 18:59:21 +0530

    fix email instance check

--- app/services/emails/email_service.py
+++ app/services/emails/email_service.py
@@ -1,6 +1,7 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from typing import Optional
 
 from app.core.config import ApiConfig
 from app.exceptions.exceptions import EmailException
@@ -9,14 +10,19 @@ from app.utils.auth_utils import AuthUtils
 from app.utils.html import account_activation_html
 
 
+class EmailConfig:
+    def __init__(self, recipient, message):
+        self.recipient = recipient
+        self.message = message
+
+
 class EmailService:
     def __init__(self, config: ApiConfig):
         self.config = config
         self.smtp_server = config.MAIL_SERVICE
         self.sender = config.MAIL_SERVICE_USER
         self.password = config.MAIL_SERVICE_PASSWORD
-        self.message = None
-        self.recipient = None
+        self.email_config: Optional[EmailConfig] = None
 
     def configure_mail(self, recipient, subject, body):
         message = MIMEMultipart()
@@ -25,18 +31,21 @@ class EmailService:
         message["Subject"] = subject
         message.attach(MIMEText(body, "html"))
 
-        self.recipient = recipient
-        self.message = message
+        self.email_config = EmailConfig(recipient=recipient, message=message)
 
     def send_mail(self):
-        if not self.recipient or not self.message:
+        if not isinstance(self.email_config, EmailConfig):
             raise EmailException("Email service is not configured")
         try:
             with smtplib.SMTP(self.smtp_server, 587) as server:
                 server.set_debuglevel(1)
                 server.starttls()
                 server.login(self.sender, self.password)
-                server.sendmail(self.sender, self.recipient, self.message.as_string())
+                server.sendmail(
+                    self.sender,
+                    self.email_config.recipient,
+                    self.email_config.message.as_string(),
+                )
         except Exception as e:
             raise EmailException(f"Error sending email: {str(e)}")
 
